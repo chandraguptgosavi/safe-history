712b4ce3cdffdda2fea16ad301788951dd51d58a
Chandragupt Gosavi <chandraguptgosavi@gmail.com>
2021-11-17 16:10:48 +0530

    service_worker triggering fixed

--- public/js/background.js
+++ public/js/background.js
@@ -1,5 +1,12 @@
-let keywords = [];
-let switchOn = true;
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.get("keywords", (items) => {
         if (typeof items.keywords === "undefined") {
@@ -8,36 +15,22 @@ chrome.runtime.onInstalled.addListener(() => {
     });
     chrome.storage.sync.set({ switchOn: true });
 });
-chrome.storage.sync.get("keywords", (items) => {
-    if (typeof items.keywords !== "undefined") {
-        keywords = items.keywords;
-    }
-});
-chrome.storage.sync.get("switchOn", (items) => {
-    if (typeof items.keywords !== "undefined") {
-        switchOn = items.switchOn;
-    }
-});
-chrome.storage.onChanged.addListener((changes, areaName) => {
-    if (areaName === "sync") {
-        if (typeof changes.keywords !== "undefined") {
-            keywords = changes.keywords.newValue;
-        }
-        if (typeof changes.switchOn !== "undefined") {
-            switchOn = changes.switchOn.newValue;
-        }
-    }
-});
-chrome.history.onVisited.addListener((result) => {
-    setTimeout(() => {
-        if (result.url && switchOn) {
-            for (let keyword of keywords) {
-                if (result.url.toLocaleLowerCase().includes(keyword.data)) {
-                    chrome.history.deleteUrl({ url: result.url });
-                    break;
+chrome.history.onVisited.addListener((result) => __awaiter(void 0, void 0, void 0, function* () {
+    chrome.storage.sync.get("switchOn", (items) => {
+        if (result.url && items.switchOn) {
+            chrome.storage.sync.get("keywords", (items) => {
+                const keywords = items.keywords;
+                if (typeof keywords !== "undefined") {
+                    for (let keyword of keywords) {
+                        if (typeof result.url !== "undefined" &&
+                            result.url.toLocaleLowerCase().includes(keyword.data)) {
+                            chrome.history.deleteUrl({ url: result.url });
+                            break;
+                        }
+                    }
                 }
-            }
+            });
         }
-    }, 0);
-});
+    });
+}));
 export {};
--- src/background.ts
+++ src/background.ts
@@ -1,8 +1,5 @@
 import { Keyword } from "./models";
 
-let keywords: Keyword[] = [];
-let switchOn: Boolean = true;
-
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.get("keywords", (items: { [key: string]: any }) => {
     if (typeof items.keywords === "undefined") {
@@ -12,38 +9,25 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ switchOn: true });
 });
 
-chrome.storage.sync.get("keywords", (items: { [key: string]: any }) => {
-  if (typeof items.keywords !== "undefined") {
-    keywords = items.keywords;
-  }
-});
-
-chrome.storage.sync.get("switchOn", (items: { [key: string]: any }) => {
-  if (typeof items.keywords !== "undefined") {
-    switchOn = items.switchOn;
-  }
-});
-
-chrome.storage.onChanged.addListener((changes: any, areaName: string) => {
-  if (areaName === "sync") {
-    if (typeof changes.keywords !== "undefined") {
-      keywords = changes.keywords.newValue;
-    }
-    if (typeof changes.switchOn !== "undefined") {
-      switchOn = changes.switchOn.newValue;
-    }
-  }
-});
-
-chrome.history.onVisited.addListener((result: chrome.history.HistoryItem) => {
-  setTimeout(() => {
-    if (result.url && switchOn) {
-      for (let keyword of keywords) {
-        if (result.url.toLocaleLowerCase().includes(keyword.data)) {
-          chrome.history.deleteUrl({ url: result.url });
-          break;
-        }
+chrome.history.onVisited.addListener(
+  async (result: chrome.history.HistoryItem) => {
+    chrome.storage.sync.get("switchOn", (items: { [key: string]: any }) => {
+      if (result.url && items.switchOn) {
+        chrome.storage.sync.get("keywords", (items: { [key: string]: any }) => {
+          const keywords: Keyword[] = items.keywords;
+          if (typeof keywords !== "undefined") {
+            for (let keyword of keywords) {
+              if (
+                typeof result.url !== "undefined" &&
+                result.url.toLocaleLowerCase().includes(keyword.data)
+              ) {
+                chrome.history.deleteUrl({ url: result.url });
+                break;
+              }
+            }
+          }
+        });
       }
-    }
-  }, 0);
-});
+    });
+  }
+);
